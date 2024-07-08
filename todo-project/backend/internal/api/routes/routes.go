3d70dd512756bc8399f5a33464d367d93a996cf5
3nd3r1 <ender.spam.guru@gmail.com>
2024-07-08 15:38:00 +0300

    exercise 1.5

--- todo-project/backend/internal/api/routes/routes.go
+++ todo-project/backend/internal/api/routes/routes.go
@@ -9,4 +9,7 @@ func SetupRoutes(router *gin.Engine) {
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Server is up and running")
 	})
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "pong")
+	})
 }
