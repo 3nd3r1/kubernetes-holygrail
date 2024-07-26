07f9d69b0b42dcb1f2f3c8e1ebc051c16d906892
3nd3r1 <ender.spam.guru@gmail.com>
2024-07-26 12:13:06 +0300

    exercise 2.3

--- delete.sh
+++ delete.sh
@@ -1,13 +1,7 @@
-kubectl delete service log-output-svc
-kubectl delete service ping-pong-svc
+kubectl delete namespace applications
+
 kubectl delete service todo-project-svc
-
-kubectl delete ingress log-output-ingress
 kubectl delete ingress todo-project-ingress
-
-kubectl delete deployment log-output-dep
-kubectl delete deployment ping-pong-dep
 kubectl delete deployment todo-project-dep
-
 kubectl delete pvc todo-project-data-pvc
 kubectl delete pv todo-project-data-pv
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-#kubectl apply -f ./ping-pong/manifests/
-#kubectl apply -f ./log-output/manifests/
+kubectl create namespace applications
+kubectl apply -f ./ping-pong/manifests/
+kubectl apply -f ./log-output/manifests/
 
-kubectl apply -f ./pvs/todo-project/
-kubectl apply -f ./todo-project/manifests/
+#kubectl apply -f ./pvs/todo-project/
+#kubectl apply -f ./todo-project/manifests/
