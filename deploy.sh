f4e88afb0926374787e7193ada6aa46f4cfb5b2f
3nd3r1 <ender.spam.guru@gmail.com>
2024-08-19 15:53:20 +0300

    exercise 3.9

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 export SOPS_AGE_KEY_FILE=./key.txt
 
-#kubectl create namespace applications
-#sops --decrypt ./ping-pong/manifests/secrets/secret.enc.yaml | kubectl apply -f -
-#kubectl apply -f ./ping-pong/manifests/
-#kubectl apply -f ./log-output/manifests/
+kubectl create namespace applications
+sops --decrypt ./ping-pong/manifests/secrets/secret.enc.yaml | kubectl apply -f -
+kubectl apply -f ./ping-pong/manifests/
+kubectl apply -f ./log-output/manifests/
 
-sops --decrypt ./todo-project/manifests/secrets/secret.enc.yaml | kubectl apply -f -
-kubectl apply -k ./todo-project/
+#sops --decrypt ./todo-project/manifests/secrets/secret.enc.yaml | kubectl apply -f -
+#kubectl apply -k ./todo-project/
